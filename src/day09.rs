c44d1981377c2de6ecdcea1d29a494e0151edce1
Reagan <areagan@massmutual.com>
2025-12-11 07:42:06 -0500

    Did it.

--- src/day09.rs
+++ src/day09.rs
@@ -18,27 +18,20 @@ fn straight_line_distance(p1: &Vec<i32>, p2: &Vec<i32>) -> i32 {
     (p1[0] - p2[0]).abs() +  (p1[1] - p2[1]).abs()
 }
 
-fn n_intersections(p: &Vec<i32>, direction: &i8, up_walls: &Vec<&Vec<&Vec<i32>>>, across_walls: &Vec<&Vec<&Vec<i32>>>) -> usize {
+fn n_intersections(p: &Vec<i32>, up_walls: &Vec<&Vec<&Vec<i32>>>) -> usize {
     // direction 0 right 1 down 2 left 3 up
     // the matching x[0][1] <= p[1] will hit the corner (end of the wall)
     // while x[0][1] < p[1] will not
     // since we dont care about corners, we'll forgo it
-    let perpendicular_walls_hit: Vec<&&Vec<&Vec<i32>>> = match direction {
-        0 => up_walls.iter().filter(|vertices| vertices[0][0] > p[0]).filter(|vertices| (vertices[0][1] < p[1] && vertices[1][1] > p[1]) || (vertices[1][1] < p[1] && vertices[0][1] > p[1])).collect(),
-        1 => across_walls.iter().filter(|vertices| vertices[0][1] > p[1]).filter(|vertices| (vertices[0][0] < p[0] && vertices[1][0] > p[0]) || (vertices[1][0] < p[0] && vertices[0][0] > p[0])).collect(),
-        2 => up_walls.iter().filter(|vertices| vertices[0][0] < p[0]).filter(|vertices| (vertices[0][1] < p[1] && vertices[1][1] > p[1]) || (vertices[1][1] < p[1] && vertices[0][1] > p[1])).collect(),
-        3 => across_walls.iter().filter(|vertices| vertices[0][1] < p[1]).filter(|vertices| (vertices[0][0] < p[0] && vertices[1][0] > p[0]) || (vertices[1][0] < p[0] && vertices[0][0] > p[0])).collect(),
-        _ => panic!("not a valid direction!"),
-    };
-    let parallel_walls_hit: Vec<&&Vec<&Vec<i32>>> = match direction {
-        0 => across_walls.iter().filter(|vertices| vertices[0][1] == p[1]).filter(|vertices| vertices[0][0] > p[0] && vertices[1][0] > p[0]).collect(),
-        1 => up_walls.iter().filter(|vertices| vertices[0][0] == p[0]).filter(|vertices| vertices[0][1] > p[1] && vertices[1][1] > p[1]).collect(),
-        2 => across_walls.iter().filter(|vertices| vertices[0][1] == p[1]).filter(|vertices| vertices[0][0] < p[0] && vertices[1][0] < p[0]).collect(),
-        3 => up_walls.iter().filter(|vertices| vertices[0][0] == p[0]).filter(|vertices| vertices[0][1] < p[1] && vertices[1][1] < p[1]).collect(),
-        _ => panic!("not a valid direction!"),
-    };
-    // println!("point {:?} going direction {} hits walls {:?} perpendicularly, and walls {:?} parallely", p, direction, perpendicular_walls_hit, parallel_walls_hit);
-    perpendicular_walls_hit.len() + parallel_walls_hit.len()
+    let perpendicular_walls_hit: Vec<&&Vec<&Vec<i32>>> = up_walls.iter()
+        .filter(|vertices| vertices[0][0] > p[0])
+        .filter(|vertices|
+            min(vertices[0][1], vertices[1][1]) < p[1] &&
+            p[1] <= max(vertices[0][1], vertices[1][1])
+        )
+        .collect();
+    // println!("point {:?} going right hits walls {:?} perpendicularly", p, perpendicular_walls_hit);
+    perpendicular_walls_hit.len()
 }
 
 fn inside_rectangle(p: &Vec<i32>, p1: &Vec<i32>, p2: &Vec<i32>) -> bool {
@@ -53,63 +46,27 @@ fn inside_rectangle(p: &Vec<i32>, p1: &Vec<i32>, p2: &Vec<i32>) -> bool {
 fn inside_shape(p: &Vec<i32>, up_walls: &Vec<&Vec<&Vec<i32>>>, across_walls: &Vec<&Vec<&Vec<i32>>>) -> bool {
     // first let's check if the point fall directly on a wall
     // that's easier to check than the ray casting?
-    for wall in up_walls.iter() {
-        if wall[0][0] == p[0] && ((wall[0][1] <= p[1] && wall[1][1] >= p[1]) || (wall[1][1] <= p[1] && wall[0][1] >= p[1])) {
-            // println!("point {:?} is on wall {:?}", p, wall);
-            return true;
-        }
-        if wall[0][1] == p[1] && ((wall[0][0] <= p[0] && wall[1][0] >= p[0]) || (wall[1][0] <= p[0] && wall[0][0] >= p[0])) {
-            // println!("point {:?} is on wall {:?}", p, wall);
-            return true;
-        }
+    let on_walls_vertical: Vec<&&Vec<&Vec<i32>>> = up_walls.iter()
+        .filter(|vertices| vertices[0][0] == p[0])
+        .filter(|vertices|
+            min(vertices[0][1], vertices[1][1]) <= p[1] &&
+            p[1] <= max(vertices[0][1], vertices[1][1])
+        )
+        .collect();
+    if on_walls_vertical.len() > 0 {
+        return true
     }
-    for wall in across_walls.iter() {
-        if wall[0][0] == p[0] && ((wall[0][1] <= p[1] && wall[1][1] >= p[1]) || (wall[1][1] <= p[1] && wall[0][1] >= p[1])) {
-            // println!("point {:?} is on wall {:?}", p, wall);
-            return true;
-        }
-        if wall[0][1] == p[1] && ((wall[0][0] <= p[0] && wall[1][0] >= p[0]) || (wall[1][0] <= p[0] && wall[0][0] >= p[0])) {
-            // println!("point {:?} is on wall {:?}", p, wall);
-            return true;
-        }
+    let on_walls_horizontal: Vec<&&Vec<&Vec<i32>>> = across_walls.iter()
+        .filter(|vertices| vertices[0][1] == p[1])
+        .filter(|vertices|
+            min(vertices[0][0], vertices[1][0]) <= p[0] &&
+            p[0] <= max(vertices[0][0], vertices[1][0])
+        )
+        .collect();
+    if on_walls_horizontal.len() > 0 {
+        return true
     }
-    // if n_intersections(p, &0, up_walls, across_walls) % 2 == 1 {
-    //     println!("point {:?} hit a single wall going right, must be inside", p);
-    //     return true;
-    // }
-    // if n_intersections(p, &1, up_walls, across_walls) % 2 == 1 {
-    //     println!("point {:?} hit a single wall going down, must be inside", p);
-    //     return true;        
-    // }
-    // if n_intersections(p, &2, up_walls, across_walls) % 2 == 1 {
-    //     println!("point {:?} hit a single wall going left, must be inside", p);
-    //     return true;        
-    // }
-    // if n_intersections(p, &3, up_walls, across_walls) % 2 == 1 {
-    //     println!("point {:?} hit a single wall going up, must be inside", p);
-    //     return true;
-    // }
-    // println!("point {:?} is not on a wall and didn't ray cast onto a single wall", p);
-    // false
-    // instead of any single hit, we check for any double hit
-    if n_intersections(p, &0, up_walls, across_walls) % 2 == 0 {
-        // println!("point {:?} hit even # of walls right, must be outside", p);
-        return false;
-    }
-    if n_intersections(p, &1, up_walls, across_walls) % 2 == 0 {
-        // println!("point {:?} hit even # of walls down, must be outside", p);
-        return false;        
-    }
-    if n_intersections(p, &2, up_walls, across_walls) % 2 == 0 {
-        // println!("point {:?} hit even # of walls left, must be outside", p);
-        return false;        
-    }
-    if n_intersections(p, &3, up_walls, across_walls) % 2 == 0 {
-        // println!("point {:?} hit even # of walls up, must be outside", p);
-        return false;
-    }
-    // !("point {:?} is not on a wall and didn't have any even # transitions", p);
-    true    
+    n_intersections(p, up_walls) % 2 == 1
 }
 
 fn vertices_to_walls(points: &Vec<Vec<i32>>) -> Vec<Vec<&Vec<i32>>> {
@@ -355,42 +312,6 @@ fn pretty_print_grid(grid: &Vec<Vec<bool>>) {
     }
 }
 
-// fn walls_to_grid(walls: &Vec<Vec<&Vec<i32>>>, max_x: i32, max_y: i32) -> Vec<Vec<bool>> {
-//     let mut grid: Vec<Vec<bool>> = vec![vec![false; (max_x+1) as usize]; (max_y+1) as usize];
-//     for wall in walls.iter() {
-//         if wall[0][0] == wall[1][0] {
-//             // vertical wall
-//             let x = wall[0][0];
-//             let y_start = min(wall[0][1], wall[1][1]);
-//             let y_end = max(wall[0][1], wall[1][1]);
-//             for y in y_start..=y_end {
-//                 grid[y as usize][x as usize] = true;
-//             }
-//         } else if wall[0][1] == wall[1][1] {
-//             // horizontal wall
-//             let y = wall[0][1];
-//             let x_start = min(wall[0][0], wall[1][0]);
-//             let x_end = max(wall[0][0], wall[1][0]);
-//             for x in x_start..=x_end {
-//                 grid[y as usize][x as usize] = true;
-//             }
-//         } else {
-//             println!("non straight wall found: {:?}", wall);
-//         }
-//     }
-//     grid
-// }
-
-// fn pretty_print_walls(walls: &Vec<Vec<&Vec<i32>>>) {
-//     // build a grid of these walls for pretty printing/debugging
-//     let grid: Vec<Vec<bool>> = walls_to_grid(
-//         walls, 
-//         walls.iter().map(|x| x[0][0]).max().unwrap_or(0), 
-//         walls.iter().map(|x| x[0][1]).max().unwrap_or(0)
-//     );
-//     pretty_print_grid(&grid);
-// }
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -409,76 +330,6 @@ mod tests {
         assert_eq!(part_1, 50);
         assert_eq!(part_2, 24);
     }
-
-    #[test]
-    fn test_intersection_count() {
-        let up_wall1 = vec![vec![10, 0], vec![10, 5]];
-        let up_wall2 = vec![vec![15, 0], vec![15, 5]];
-        let across_wall1 = vec![vec![0, 5], vec![20, 5]];
-        let across_wall2 = vec![vec![0, 10], vec![20, 10]];
-        let up_wall1_inner = up_wall1.iter().collect();
-        let up_wall2_inner = up_wall2.iter().collect();
-        let across_wall1_inner = across_wall1.iter().collect();
-        let across_wall2_inner = across_wall2.iter().collect();
-        let up_walls: Vec<&Vec<&Vec<i32>>> = vec![
-            &up_wall1_inner,
-            &up_wall2_inner,
-        ];
-        let across_walls: Vec<&Vec<&Vec<i32>>> = vec![
-            &across_wall1_inner,
-            &across_wall2_inner,
-        ];
-        let above_right = n_intersections(&vec![9, 0].to_vec(), &(0 as i8), &up_walls, &across_walls);
-        assert_eq!(above_right, 0);
-        let below_right = n_intersections(&vec![9, 5].to_vec(), &(0 as i8), &up_walls, &across_walls);
-        assert_eq!(below_right, 0);
-        let inside_right = n_intersections(&vec![11, 1].to_vec(), &(0 as i8), &up_walls, &across_walls);
-        assert_eq!(inside_right, 1);
-        let outside_right = n_intersections(&vec![16, 1].to_vec(), &(0 as i8), &up_walls, &across_walls);
-        assert_eq!(outside_right, 0);
-        let cross_right = n_intersections(&vec![9, 1].to_vec(), &(0 as i8), &up_walls, &across_walls);
-        assert_eq!(cross_right, 2);        
-        let inside_left = n_intersections(&vec![11, 1].to_vec(), &(2 as i8), &up_walls, &across_walls);
-        assert_eq!(inside_left, 1);
-        let cross_left = n_intersections(&vec![16, 1].to_vec(), &(2 as i8), &up_walls, &across_walls);
-        assert_eq!(cross_left, 2);
-        let outside_left = n_intersections(&vec![9, 1].to_vec(), &(2 as i8), &up_walls, &across_walls);
-        assert_eq!(outside_left, 0);
-        let inside_down = n_intersections(&vec![1, 6].to_vec(), &(1 as i8), &up_walls, &across_walls);
-        assert_eq!(inside_down, 1);
-        let cross_down = n_intersections(&vec![1, 4].to_vec(), &(1 as i8), &up_walls, &across_walls);
-        assert_eq!(cross_down, 2);
-        let outside_down = n_intersections(&vec![1, 11].to_vec(), &(1 as i8), &up_walls, &across_walls);
-        assert_eq!(outside_down, 0);
-        let inside_up = n_intersections(&vec![1, 6].to_vec(), &(3 as i8), &up_walls, &across_walls);
-        assert_eq!(inside_up, 1);
-        let outside_up = n_intersections(&vec![1, 4].to_vec(), &(3 as i8), &up_walls, &across_walls);
-        assert_eq!(outside_up, 0);
-        let cross_up = n_intersections(&vec![1, 11].to_vec(), &(3 as i8), &up_walls, &across_walls);
-        assert_eq!(cross_up, 2);
-    }
-
-    #[test]
-    fn test_intersection_count_spot() {
-        let up_wall1 = vec![vec![10, 0], vec![10, 5]];
-        let up_wall2 = vec![vec![15, 0], vec![15, 5]];
-        let across_wall1 = vec![vec![2, 3], vec![7, 3]];
-        let across_wall2 = vec![vec![9, 5], vec![2, 5]];
-        let up_wall1_inner = up_wall1.iter().collect();
-        let up_wall2_inner = up_wall2.iter().collect();
-        let across_wall1_inner = across_wall1.iter().collect();
-        let across_wall2_inner = across_wall2.iter().collect();
-        let up_walls: Vec<&Vec<&Vec<i32>>> = vec![
-            &up_wall1_inner,
-            &up_wall2_inner,
-        ];
-        let across_walls: Vec<&Vec<&Vec<i32>>> = vec![
-            &across_wall1_inner,
-            &across_wall2_inner,
-        ];
-        let cross_down = n_intersections(&vec![3, 0].to_vec(), &(1 as i8), &up_walls, &across_walls);
-        assert_eq!(cross_down, 2);
-    }    
 
     #[test]
     fn test_inside_shape() {
@@ -521,7 +372,6 @@ mod tests {
             for test_x in 0..expected[0].len() {
                 let test_point = vec![test_x as i32, test_y as i32];
                 tmp_vec.push(inside_shape(&test_point, &up_walls, &across_walls));
-                // assert_eq!(inside_shape(test_point, &up_walls, &across_walls), full_results[test_y][test_x]);
             }
             result.push(tmp_vec);
         }
@@ -532,29 +382,4 @@ mod tests {
             }
         }
     }
-
-//     #[test]
-//     fn test_inside_shape_2() {
-//         let input = "1,1
-// 5,1
-// 5,5
-// 3,5
-// 3,3
-// 1,3";
-//         let lines: Vec<&str> = input.lines().collect();
-//         let points: Vec<Vec<i32>> = lines.iter()
-//             .map(|x| {
-//                 x.split(",")
-//                     .map(|n| n.parse::<i32>().expect("should parse to int"))
-//                     .collect()
-//             })
-//             .collect();
-//         let walls = vertices_to_walls(&points);
-//         let (up_walls, across_walls) = split_walls(&walls);
-//         // convert this full map into T/F for where we're inside
-
-//         pretty_print_walls(&walls);
-//         assert_eq!(true , false);
-//     }
-
 }
